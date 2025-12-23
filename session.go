619a6d9ca4fa885cdd68f8aba245c75d08980d76
Streamlet <streamlet@outlook.com>
2025-12-24 00:57:07 +0800

    keep session alive

--- session.go
+++ session.go
@@ -36,6 +36,7 @@ type sessionManager struct {
 
 func (sm *sessionManager) GetSession(sessionId string) Session {
 	if sm.cacheProvider.Exists(sessionId) {
+		sm.cacheProvider.HSet(sessionId, "", nil, time.Minute*DefaultSessionExpireMinutes)
 		return newSession(sessionId, sm.cacheProvider)
 	} else {
 		return nil
